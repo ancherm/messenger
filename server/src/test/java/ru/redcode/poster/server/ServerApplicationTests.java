f7d09d1a6095ca9884e9bde962d7febbcee8690c
ancherm <andrewcherm2014@gmail.com>
2026-02-25 10:20:19 +0400

    init server

---
+++ server/src/test/java/ru/redcode/poster/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.redcode.poster.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
